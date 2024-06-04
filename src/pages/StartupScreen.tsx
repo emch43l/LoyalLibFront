ec1da092464b2cd15ceb8b30671e5285721e24f5
emch43l <michalgood10@gmail.com>
2024-06-04 12:37:24 +0200

    added authors modal

--- src/pages/StartupScreen.tsx
+++ src/pages/StartupScreen.tsx
@@ -1,9 +1,13 @@
 import { useNavigation } from "@react-navigation/native";
-import { Button, View, Text, Pressable } from "react-native";
+import { Button, View, Text, Pressable, Modal, Image } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import { useState } from "react";
+import { useAssets } from "expo-asset";
 
 export default function StartupScreen() {
   const navigation = useNavigation();
+  const [open, setOpen] = useState<boolean>(false);
+  const [assets, error] = useAssets([require("../../assets/wsei-logo.png")]);
 
   return (
     <LinearGradient
@@ -32,8 +36,47 @@ export default function StartupScreen() {
               </Text>
             </Pressable>
           </View>
+          <View>
+            <Pressable onPress={() => setOpen(true)}>
+              <Text className="text-center mt-2 text-white font-bold underline text-lg uppercase">
+                Twórcy
+              </Text>
+            </Pressable>
+          </View>
         </View>
       </View>
+      <Modal
+        animationType="slide"
+        transparent={false}
+        visible={open}
+        onRequestClose={() => {
+          setOpen(false);
+        }}
+      >
+        <View>
+          <View className="mt-8">
+            {assets ? (
+              <Image
+                className="h-[80]"
+                resizeMode="contain"
+                source={{ uri: assets[0].uri }}
+              />
+            ) : null}
+            <View className="flex items-center">
+              <Text className="mt-8 mb-4 font-bold text-2xl">Autorzy</Text>
+              <Text>Michał Mierzwa</Text>
+              <Text>Tomasz Michniak</Text>
+            </View>
+
+            <Pressable
+              className="mt-20 border-2 border-black flex items-center w-[50%] mx-auto rounded-md bg-gray-200 py-2"
+              onPress={() => setOpen(false)}
+            >
+              <Text className="font-bold">Zamknij</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
     </LinearGradient>
   );
 }
